374b77d807a55a2866d315fd58703013a74ef348
DarccMan <darcy3057@gmail.com>
2022-06-25 15:16:00 +1000

    Add test message

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
 import "./App.scss";
 
 export default function App() {
-  return <div className="App">VSV Chat</div>;
+  const [testMsg, setTestMsg] = useState<string | null>(null);
+  useEffect(() => {
+    fetch(process.env.REACT_APP_API!)
+      .then(res => res.blob())
+      .then(blob => blob.text())
+      .then(text => {
+        setTestMsg(text);
+      })
+      .catch(err => {
+        throw err;
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>VSV Chat</h1>
+      <p>Test: {testMsg || "..."}</p>
+    </div>
+  );
 }
